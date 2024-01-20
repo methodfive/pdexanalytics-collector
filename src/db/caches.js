cc18e34c58d1053e18640916eb7f095444df4b35
Jason Mansfield <jmansfield@gmail.com>
2024-01-19 20:34:39 -0500

    bug fix for asset 24h query to due with quote assets

--- src/db/caches.js
+++ src/db/caches.js
@@ -31,30 +31,42 @@ ON DUPLICATE KEY UPDATE trades=coalesce(ag1.trades,0), volume=coalesce(ag1.volum
         );
 
         await queryAsyncWithRetries(connectionPool,
-            `insert into assets_24h(asset_id, tvl, price, balance, volume, trades, previous_tvl, previous_price, previous_balance, previous_volume, previous_trades) 
+            `
+insert into assets_24h(asset_id, tvl, price, balance, volume, trades, previous_tvl, previous_price, previous_balance, previous_volume, previous_trades)  
+
 select assets.asset_id, coalesce(assets.tvl, 0) as tvl, coalesce(assets.price,0) as price, assets.balance, coalesce(ag1.volume,0) as volume, coalesce(ag1.trades,0) as trades, 
-coalesce(previous.tvl,0) as previous_tvl, coalesce(previous.price,0) as previous_price, coalesce(previous.balance,0) as previous_balance, coalesce(ag2.prev_volume, 0) as previous_volume,  coalesce(ag2.prev_trades, 0) as previous_trades 
+coalesce(previous.tvl,0) as previous_tvl, coalesce(previous.price,0) as previous_price, coalesce(previous.balance,0) as previous_balance, coalesce(ag2.volume, 0) as previous_volume,  coalesce(ag2.trades, 0) as previous_trades 
  from assets 
  left outer join (
-   select max(stat_time) as stat_time, asset_id from assets_hourly 
+ select max(stat_time) as stat_time, asset_id from assets_hourly 
    where assets_hourly.stat_time <= DATE_SUB(CURTIME(), INTERVAL 1 DAY) group by asset_id) 
 previous_stat_time on previous_stat_time.asset_id = assets.asset_id
  left outer join assets_hourly as previous on previous.asset_id = assets.asset_id and previous.stat_time = previous_stat_time.stat_time 
  left outer join (
-                select count(*) as trades,sum(volume) as volume, base_asset_id, quote_asset_id from trades
-                where timestamp > DATE_SUB(NOW(), INTERVAL 24 HOUR) 
-                group by base_asset_id, quote_asset_id 
-                ) ag1 on ag1.base_asset_id = assets.asset_id or ag1.quote_asset_id = assets.asset_id 
+                select count(*) as trades,sum(volume) as volume, base_asset_id as asset_id from trades
+                where timestamp > DATE_SUB(NOW(), INTERVAL 24 HOUR) group by base_asset_id
+
+                union
+                
+                select count(*) as trades,sum(volume) as volume, quote_asset_id as asset_id from trades
+                where timestamp > DATE_SUB(NOW(), INTERVAL 24 HOUR) group by quote_asset_id
+) ag1 on ag1.asset_id = assets.asset_id 
  left outer join (
-                select count(*) as prev_trades,sum(volume) as prev_volume, base_asset_id, quote_asset_id from trades
+                 select count(*) as trades,sum(volume) as volume, base_asset_id as asset_id from trades
                 where timestamp < DATE_SUB(NOW(), INTERVAL 24 HOUR) and
-                timestamp > DATE_SUB(NOW(), INTERVAL 48 HOUR) 
-                group by base_asset_id, quote_asset_id 
-                ) ag2 on ag2.base_asset_id = assets.asset_id or ag2.quote_asset_id = assets.asset_id
+                timestamp > DATE_SUB(NOW(), INTERVAL 48 HOUR)  group by base_asset_id
+
+                union
+                
+                select count(*) as trades,sum(volume) as volume, quote_asset_id as asset_id from trades
+                where timestamp < DATE_SUB(NOW(), INTERVAL 24 HOUR) and
+                timestamp > DATE_SUB(NOW(), INTERVAL 48 HOUR)  group by quote_asset_id
+) ag2 on ag2.asset_id = assets.asset_id
 ON DUPLICATE KEY 
 UPDATE tvl=coalesce(assets.tvl, 0), price = coalesce(assets.price,0), balance = assets.balance, volume = coalesce(ag1.volume,0), 
 trades = coalesce(ag1.trades,0),  previous_tvl = coalesce(previous.tvl,0), previous_price = coalesce(previous.price,0), 
-previous_balance = coalesce(previous.balance,0),previous_volume = coalesce(ag2.prev_volume, 0), previous_trades = coalesce(ag2.prev_trades, 0)
+previous_balance = coalesce(previous.balance,0),previous_volume = coalesce(ag2.volume, 0), previous_trades = coalesce(ag2.trades, 0)
+
 `,
             [],
             ([rows,fields]) => {},
